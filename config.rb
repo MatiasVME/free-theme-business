685ba691b06b15d0f5c3b6602a309c43d7b6043c
Matías Muñoz Espinoza <writkas2@gmail.com>
2012-11-20 22:17:00 -0300

    + quite "/css" de .gitignore + otros mini-cambios

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ images_dir = "img"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :expanded # or :nested or :compact or :compressed
+output_style = :compressed # :expanded  or :nested or :compact or :compressed
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
